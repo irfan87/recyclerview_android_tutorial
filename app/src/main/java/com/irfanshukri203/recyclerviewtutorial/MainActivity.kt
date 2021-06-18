128a54ef3ab39e8eb76c75400b9ec1bcc2623346
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-18 15:00:39 +0800

    create a simple todo list with recyclerview

--- app/src/main/java/com/irfanshukri203/recyclerviewtutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/recyclerviewtutorial/MainActivity.kt
@@ -2,10 +2,27 @@ package com.irfanshukri203.recyclerviewtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val todoList = mutableListOf<Todo>()
+
+        val adapter = TodoAdapter(todoList)
+
+        rvTodos.adapter = adapter
+        rvTodos.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoText.text.toString()
+            val todo = Todo(todoTitle, false)
+
+            todoList.add(todo)
+            adapter.notifyItemInserted(todoList.size - 1)
+        }
     }
 }
---
+++ app/src/main/java/com/irfanshukri203/recyclerviewtutorial/Todo.kt
@@ -0,0 +1,6 @@
+package com.irfanshukri203.recyclerviewtutorial
+
+data class Todo(
+    val title: String,
+    val isChecked: Boolean
+)
